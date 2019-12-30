337bdc2de4d2b0f7f7f1f2173d9c0a7e8896267d
woghks2045 <woghks2045@gmail.com>
2019-12-30 19:45:20 +0900

    식품등록 기능 제작중

---
+++ src/main/java/com/ward/savefood/food/controller/FoodRestController.java
@@ -0,0 +1,46 @@
+package com.ward.savefood.food.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ward.savefood.common.controller.GeneralController;
+import com.ward.savefood.food.model.SelectFoodRequest;
+import com.ward.savefood.food.service.UserFoodService;
+import com.ward.savefood.member.service.MemberService;
+import com.ward.savefood.admin.model.UpdateCustomerRequest;
+import com.ward.savefood.admin.service.CategoryService;
+import com.ward.savefood.admin.service.CustomerService;;
+
+@RestController
+@RequestMapping("/api/food")
+public class FoodRestController extends GeneralController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	@Autowired
+	private UserFoodService foodService;
+	
+	@PostMapping("/food")
+	public ResponseEntity<?> getFoodList(@Valid @RequestBody SelectFoodRequest foodRequest, BindingResult bindingResult, HttpServletRequest request) {
+		logger.info("userFoodRestController getFoodList : "+ foodRequest.toString());
+		
+		if(bindingResult.hasErrors()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return foodService.getFoodList(foodRequest);
+	}
+	
+}
---
+++ src/main/java/com/ward/savefood/food/service/UserFoodService.java
@@ -0,0 +1,108 @@
+package com.ward.savefood.food.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ward.savefood.admin.dao.FoodDao;
+import com.ward.savefood.admin.model.InsertCategoryRequest;
+import com.ward.savefood.admin.model.InsertFoodRequest;
+import com.ward.savefood.admin.model.UpdateCategoryRequest;
+import com.ward.savefood.admin.model.UpdateFoodRequest;
+import com.ward.savefood.food.dao.UserFoodDao;
+import com.ward.savefood.food.model.SelectFoodRequest;
+
+@Service
+public class UserFoodService {
+	
+	@Autowired
+	private UserFoodDao foodDao;
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+	
+	private DefaultTransactionDefinition def = new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED);
+	
+	public ResponseEntity<?> getFoodList(SelectFoodRequest foodRequest) {	
+		try {	
+
+			Map<String, Object> selectFood = new HashMap<>();
+			selectFood.put("categorySeq", foodRequest.getCategorySeq());
+			selectFood.put("searchText", foodRequest.getSearchText());
+			ArrayList<Map<String, Object>> selectResult = foodDao.getFoodList(selectFood);
+			
+			if(selectResult.size() > 0) {
+				ArrayList<Map<String, Object>> foodList = new ArrayList<>();
+				ArrayList<ArrayList<Map<String, Object>>> foodListList = new ArrayList<>();
+				
+				for(int i = 0; i<selectResult.size(); i++) {
+					if(i == selectResult.size()-1 || selectResult.get(i).get("category_seq") != selectResult.get(i+1).get("category_seq"))
+					{
+						foodList.add(selectResult.get(i));
+						@SuppressWarnings("unchecked")
+						ArrayList<Map<String, Object>> temp = (ArrayList<Map<String, Object>>) foodList.clone();
+						foodListList.add(temp);
+						foodList.clear();
+					}
+					else {
+						foodList.add(selectResult.get(i));
+					}
+				}
+				
+				return new ResponseEntity<>(foodListList, HttpStatus.OK);
+			}
+			
+			return new ResponseEntity<>("no select result", HttpStatus.NO_CONTENT);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<>("fail to select food list", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	public ArrayList<ArrayList<Map<String, Object>>> getAllFoodList() {	
+		try {	
+
+			ArrayList<Map<String, Object>> selectResult = foodDao.getAllFoodList();
+			ArrayList<Map<String, Object>> foodList = new ArrayList<>();
+			ArrayList<ArrayList<Map<String, Object>>> foodListList = new ArrayList<>();
+			
+			for(int i = 0; i<selectResult.size(); i++) {
+				if(i == selectResult.size()-1 || selectResult.get(i).get("category_seq") != selectResult.get(i+1).get("category_seq"))
+				{
+					foodList.add(selectResult.get(i));
+					@SuppressWarnings("unchecked")
+					ArrayList<Map<String, Object>> temp = (ArrayList<Map<String, Object>>) foodList.clone();
+					foodListList.add(temp);
+					foodList.clear();
+				}
+				else {
+					foodList.add(selectResult.get(i));
+				}
+			}
+			
+			return foodListList;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+}
